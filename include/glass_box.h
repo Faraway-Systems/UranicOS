97eaf3cf5003a264de4f5b34ed08b26b078d7cdb
Faraway-Systems <neversafefrombytin@gmail.com>
2026-04-24 14:50:21 +0200

    Update glass_box.h

--- include/glass_box.h
+++ include/glass_box.h
@@ -1,1 +1,18 @@
-//to be filed
+/* * The Glass Box Mapping Header
+ * Purpose: Transparency for all kernel syscalls and binary blobs.
+ */
+
+// Original work by FlocreeperBytin in 2026
+
+#ifndef GLASS_BOX_H
+#define GLASS_BOX_H
+
+// Mandatory Syscall Mapping
+#define LFISH_SYSCALL_IPC_SEND 0x01
+#define LFISH_SYSCALL_IPC_RECV 0x02
+
+// All proprietary "blobs" must be mapped here per License Clause 8.
+
+#endif 
+
+// Original work by FlocreeperBytin in 2026 \\
