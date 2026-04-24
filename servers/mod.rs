10a9eb605c6b932955204a59e413819d7855a604
Faraway-Systems <neversafefrombytin@gmail.com>
2026-04-24 14:50:39 +0200

    Update mod.rs

--- servers/mod.rs
+++ servers/mod.rs
@@ -1,1 +1,13 @@
-//to be filed
+/* * LargeFish System Services
+ * Language: Rust
+ */
+
+// Original work by FlocreeperBytin in 2026
+
+pub fn init_subsystems() {
+    // Rust-based memory safe drivers go here.
+    // They communicate with the C kernel via IPC.
+    println!("LargeFish Services Initialized.");
+}
+
+// Original work by FlocreeperBytin in 2026 \\
